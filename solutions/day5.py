bf3b4f92a0be85d74e803f9c20d3da1722b10d58
ajtoms <toms@unbc.ca>
2023-12-04 21:45:34 -0800

    Solved D5P1

--- solutions/day5.py
+++ solutions/day5.py
@@ -14,24 +14,78 @@ except:
 	input_lines = ["Input Lines Not Found"]
 	example_lines = ["Example"]
 
+class almanac:
+
+	def __init__(self, almanac_lines) -> None:
+		seed_def = almanac_lines[0]
+		self.seeds = [int(n) for n in seed_def.replace("seeds: ", "").split()]
+
+		# Store the to, from states being transitioned in each functions, and 
+		# then interpret the ranges.
+		function_defs = []
+		new_fn = True
+		for line in almanac_lines[2:]:
+			# First line after an empty newline (or first line in loop) inits fn
+			if new_fn:
+				new_fn = False
+				line = line.replace(" map:", "")
+				orig, dest = line.split("-to-")
+				function_defs.append({"orig": orig, "dest": dest, "ranges":[]})
+				continue
+			
+			# Empty line means our function is now done. Flag appropriately
+			elif line == "":
+				new_fn = True
+				continue
+
+			# Otherwise, we just store the range as ints
+			range_vals = tuple([int(n) for n in line.split()])
+			function_defs[-1]["ranges"].append(range_vals)
+		
+		# Map the current state to the function that transforms it into it's new
+		# state and value
+		self.lookup = {}
+		for fdef in function_defs:
+			self.lookup[fdef["orig"]] = self.genfn(fdef["dest"], fdef["ranges"])
+		
+	def genfn(self, new_state, ranges):
+		# Formatting: Dest_Range_Start, Source_Range_Start, Range_Length
+		def transform(n):
+			new_n = n
+			for dest_start, src_start, range_len in ranges:
+				if n >= src_start and n < src_start + range_len:
+					delta = n - src_start
+					new_n = dest_start + delta
+					break
+			return (new_state, new_n)	# Defaults to identity if not in a range
+		return transform
+
 def do_part_one_for(lines):
-	pass
+	elfmanac = almanac(lines)
+	best = 999999999999999999
+	for val in elfmanac.seeds:
+		state = "seed"
+		while state != "location":
+			state, val = elfmanac.lookup[state](val)
+		best = min(best, val)
+	return best
 
 def do_part_two_for(lines):
 	pass
 
 def solve_p1():
 	print(f"PART ONE\n--------\n")
-	print(f"This is the prompt for Part One of the problem.\n")
+	print(f"Given a bizarre almanac as an input, what is the lowest location nu"
+	   	  f"mber one of our starting seeds ends up in?\n")
 
 	results = do_part_one_for(example_lines)
 	print(f"When we do part one for the example input:")
-	print(f"\tThe <THING THEY WANT> is {results}")
-	print(f"\tWe expected: <SOLUTION THEY WANT>\n")
+	print(f"\tThe lowest location number is {results}")
+	print(f"\tWe expected: 35\n")
 
 	results = do_part_one_for(input_lines)
 	print(f"When we do part one for the actual input:")
-	print(f"\tThe <THING THEY WANT> is {results}\n")
+	print(f"\tThe lowest location number is {results}\n")
 
 def solve_p2():
 	return
@@ -48,4 +102,4 @@ def solve_p2():
 	print(f"\tThe <THING THEY WANT> is {results}\n")
 
 def print_header():
-	print("--- DAY 5: <TITLE GOES HERE> ---\n")
+	print("--- DAY 5: If You Give a Seed a Ferilizer ---\n")
