dc526c7c65f7009d8a66391354d20822666729d8
ajtoms <toms@unbc.ca>
2023-12-17 09:41:16 -0800

    Part 1 solutions for 16/17

--- solutions/day16.py
+++ solutions/day16.py
@@ -14,11 +14,122 @@ except:
 	input_lines = ["Input Lines Not Found"]
 	example_lines = ["Example"]
 
-def do_part_one_for(lines):
-	pass
+DEBUG_MODE = False
+
+def do_part_one_for(lines, start_pos=vector(), start_dir=vector.RIGHT()):
+	VR, VD, VL, VU = vector.RIGHT(), vector.DOWN(), vector.LEFT(), vector.UP()
+	
+	gridmap = algos.vector_map_from_string_list(lines, default_fn=lambda: "#")
+	energized = {v: False for v in gridmap.keys()}
+
+	visited = set()
+	search_space = deque([(start_pos, start_dir)])
+
+	def enqueue_if_legal(position, direction):
+		# Helper function handles pruning branches if visited or dead end walls
+		next = position + direction
+		if gridmap[next] == "#" or (next, direction) in visited:
+			return
+		search_space.append((next, direction))
+
+	while len(search_space):
+		position, direction = search_space.popleft()
+		visited.add((position, direction))
+
+		# Energize the new cell. 
+		energized[position] = True
+
+		# Decide on movement to next cell based on the structure in this cell
+		current_tile_type = gridmap[position]
+
+		# No structure
+		if current_tile_type == ".":
+			enqueue_if_legal(position, direction)
+		
+		# L/R Splitter
+		elif current_tile_type == "-":
+			# Moving parallel to the splitter's type is just normal movement
+			if direction in (VR, VL):
+				enqueue_if_legal(position, direction)
+
+			# Whereas orthoganl movement becomes two moves: left, right
+			else:
+				enqueue_if_legal(position, VL)
+				enqueue_if_legal(position, VR)
+		
+		# U/D Splitter
+		elif current_tile_type == "|":
+			# Moving parallel to the splitter's type is just normal movement
+			if direction in (VU, VD):
+				enqueue_if_legal(position, direction)
+
+			# Whereas orthoganl movement becomes two moves: up, down
+			else:
+				enqueue_if_legal(position, VU)
+				enqueue_if_legal(position, VD)
+		
+	
+		# Left-hand-tilted mirror
+		elif current_tile_type == "\\":
+			if direction == VR:
+				direction = VD
+			elif direction == VU:
+				direction = VL
+			elif direction == VD:
+				direction = VR
+			elif direction == VL:
+				direction = VU
+			enqueue_if_legal(position, direction)
+		
+		# Right-hand-tilted mirror is the opposite
+		elif current_tile_type == "/":
+			if direction == VR:
+				direction = VU
+			elif direction == VU:
+				direction = VR
+			elif direction == VD:
+				direction = VL
+			elif direction == VL:
+				direction = VD
+			enqueue_if_legal(position, direction)
+
+		if DEBUG_MODE:
+			printed = gridmap.copy()
+			for v in energized.keys():
+				if energized[v]:
+					printed[v] = "O"
+			algos.print_map(printed)
+			input()
+	
+	return len([v for v in energized.keys() if energized[v]])
 
 def do_part_two_for(lines):
-	pass
+	best = 0
+
+	MAX_Y = len(lines)
+	MAX_X = len(lines[0])
+	for x in range(MAX_X):
+		start_pos_top = vector(x, 0)
+		start_dir_top = vector.DOWN()
+		best = max(best, do_part_one_for(lines, start_pos_top, start_dir_top))
+
+		start_pos_bot = vector(x, MAX_Y)
+		start_dir_bot = vector.UP()
+		do_part_one_for(lines, start_pos_bot, start_dir_bot)
+		best = max(best, do_part_one_for(lines, start_pos_bot, start_dir_bot))
+
+	for y in range(MAX_Y):
+		start_pos_lft = vector(0, y)
+		start_dir_lft = vector.RIGHT()
+		best = max(best, do_part_one_for(lines, start_pos_lft, start_dir_lft))
+
+		start_pos_rgt = vector(MAX_X, y)
+		start_dir_rgt = vector.UP()
+		do_part_one_for(lines, start_pos_rgt, start_dir_rgt)
+		best = max(best, do_part_one_for(lines, start_pos_rgt, start_dir_rgt))
+
+	return best
+
 
 def solve_p1():
 	print(f"PART ONE\n--------\n")
@@ -26,26 +137,25 @@ def solve_p1():
 
 	results = do_part_one_for(example_lines)
 	print(f"When we do part one for the example input:")
-	print(f"\tThe <THING THEY WANT> is {results}")
-	print(f"\tWe expected: <SOLUTION THEY WANT>\n")
+	print(f"\tThe energized tiles in the grid add to give {results}")
+	print(f"\tWe expected: 46\n")
 
 	results = do_part_one_for(input_lines)
 	print(f"When we do part one for the actual input:")
-	print(f"\tThe <THING THEY WANT> is {results}\n")
+	print(f"\tThe energized tiles in the grid add to give {results}\n")
 
 def solve_p2():
-	return
 	print(f"PART TWO\n--------\n")
 	print(f"This is the prompt for Part Two of the problem.\n")
 
 	results = do_part_two_for(example_lines)
 	print(f"When we do part two for the example input:")
-	print(f"\tThe <THING THEY WANT> is {results}")
-	print(f"\tWe expected: <SOLUTION THEY WANT>\n")
+	print(f"\tThe best energy results from any starting postion is {results}")
+	print(f"\tWe expected: 51\n")
 
 	results = do_part_two_for(input_lines)
 	print(f"When we do part two for the actual input:")
-	print(f"\tThe <THING THEY WANT> is {results}\n")
+	print(f"\tThe best energy results from any starting postion is {results}\n")
 
 def print_header():
-	print("--- DAY 16: <TITLE GOES HERE> ---\n")
+	print("--- DAY 16: The Floor Will Be Lava ---\n")
--- solutions/day17.py
+++ solutions/day17.py
@@ -7,6 +7,8 @@ import utilities.io as io
 from collections import defaultdict
 from collections import deque
 
+import heapq
+
 try:
 	input_lines = io.read_input_as_lines(17)
 	example_lines = io.read_example_as_lines(17)
@@ -14,24 +16,105 @@ except:
 	input_lines = ["Input Lines Not Found"]
 	example_lines = ["Example"]
 
+class SNode:
+
+	best_visits = defaultdict(lambda: 999999999)
+
+	def __init__(self, posn: vector, running_heat, path, grid) -> None:
+		self.posn = posn
+		self.running_heat = running_heat
+		self.path = path
+		self.grid = grid
+		SNode.best_visits[posn] = min(SNode.best_visits[posn], running_heat)
+
+	def neighbours(self):
+		can_go_straight = True
+		if len(self.path) >= 3:
+			last_3_moves = set(self.path[-3:])
+			can_go_straight = len(last_3_moves) > 1
+		
+		last_dir = self.path[-1] if len(self.path) else vector.RIGHT()
+		options = [last_dir.rotate_ccw(1), last_dir.rotate_cw(1)]
+		if can_go_straight:
+			options.insert(0, last_dir)
+
+		neighbours = []
+		for opt in options:
+			spec = opt + self.posn
+			if SNode.best_visits[spec] < self.running_heat + self.grid[spec]:
+				continue
+			spec_heat = self.running_heat + self.grid[spec]
+			spec_path = self.path.copy()
+			spec_path.append(opt)
+			neighbours.append(SNode(spec, spec_heat, spec_path, self.grid))
+		return neighbours
+		
+
+
+
 def do_part_one_for(lines):
-	pass
+	df = lambda: 999999999
+	gridmap = algos.vector_map_from_string_list(lines, default_fn=df, 
+											 	interpreter_fn=int)
+	
+	orig = vector()
+	dest = vector(len(lines[0]) - 1, len(lines) - 1)
+
+	# Boiler-plate state tracking. 
+	ENTRIES = 0
+	unvisited = set(gridmap.keys())
+	search_queue = []
+	def extend_search_queue(new_nodes):
+		nonlocal ENTRIES, unvisited, search_queue
+		
+		# Assumes we're extending by a list, but can handle extending by a node.
+		if not isinstance(new_nodes, list):
+			new_nodes = [new_nodes]
+
+		# Including ENTRIES as a tiebreaking value maintains the heap invariant
+		for node in new_nodes:
+			node: SNode
+			search_queue_node = (node.running_heat, ENTRIES, node)
+			heapq.heappush(search_queue, search_queue_node)
+			ENTRIES += 1
+
+	# Initializing values
+	extend_search_queue(SNode(orig, 0, [], gridmap))
+
+	# Search the entire space!
+	while len(search_queue):
+		# dist and tiebreaker are needed by the heap for sorting, but not by us
+		dist, tiebreaker, node = heapq.heappop(search_queue)
+		node:SNode
+
+		# Count this as a visit and update our records if we need to. 
+		if node.posn == dest:
+			continue
+		
+		# If we get here, we need to continue pathfinding.
+		extend_search_queue(node.neighbours())
+	
+	return SNode.best_visits[dest]
+			
+
 
 def do_part_two_for(lines):
 	pass
 
 def solve_p1():
 	print(f"PART ONE\n--------\n")
-	print(f"This is the prompt for Part One of the problem.\n")
+	print(f"Shortest path through grid where you can't go backwards, only left,"
+	   	  f" right or forwards, and you can only go 3 steps forward before a tu"
+		  f"rn.\n")
 
 	results = do_part_one_for(example_lines)
 	print(f"When we do part one for the example input:")
-	print(f"\tThe <THING THEY WANT> is {results}")
-	print(f"\tWe expected: <SOLUTION THEY WANT>\n")
+	print(f"\tThe least possible heat loss is {results}")
+	print(f"\tWe expected: 102\n")
 
 	results = do_part_one_for(input_lines)
 	print(f"When we do part one for the actual input:")
-	print(f"\tThe <THING THEY WANT> is {results}\n")
+	print(f"\tThe least possible heat loss is {results}\n")
 
 def solve_p2():
 	return
