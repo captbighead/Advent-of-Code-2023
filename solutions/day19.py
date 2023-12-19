6d655ceb1beff568113eb90ef288fb00eb61fbaa
captbighead <ajt2cbhds@gmail.com>
2023-12-19 13:06:18 -0800

    Solved Day 19 P2

--- solutions/day19.py
+++ solutions/day19.py
@@ -75,18 +75,78 @@ def do_part_one_for(lines):
 		summation += sum(part.values())
 	return summation
 
-class tree_node:
+class graph_node:
 
-	def __init__(self) -> None:
-		
-		# Make a tree, of the decisions, take 4 tuples (0:4000) inclusive, bring
-		# them down through the tree until they reach a leaf node. For every 
-		# leaf node: mutiply the cardinalities of the tuples against each other
-		# to get that leaf node's possibility space. Multiply the leaf nodes' 
-		# spaces together, bada bing bada boom 
+	def __init__(self, name, check_num, def_str, registry) -> None:
+		self.name = name
+		self.check_num = check_num
+		self.registry = registry
+
+		if def_str.find(":") != -1:
+			self.checked_id = def_str[0]
+			def_val, def_dest = def_str[2:].split(":")
+			def_val = int(def_val)
+			self.upper = 4000 if def_str[1] == ">" else def_val - 1
+			self.lower = 1 if def_str[1] == "<" else def_val + 1
+			self.dest_true = (def_dest, 0)
+			self.dest_false = (name, check_num + 1)
+		else:
+			# Could check against anything, it all goes to the same place.
+			self.checked_id = "x"	
+			self.upper = 4000
+			self.lower = 1
+			self.dest_true = (def_str, 0)
+			self.dest_false = (def_str, 0)
+
+		registry[(name, check_num)] = self
+
+def split_ranges(node, xmas):
+	comb = tuple(sorted(node + xmas))
+
+	# If the second element is the maximum value of either range, then the two
+	# ranges are disjoint. Otherwise, the second element is our lower-bound for
+	# the intersection and the third is our upper bound. 
+	inner = tuple(comb[1:3]) if comb[1] not in (node[1], xmas[1]) else None
+
+	# The exception to the above rule is if they intersect by exactly one 
+	# element, so check for that just in case:
+	if inner == None and comb[1] == comb[2]:
+		inner = (comb[1], comb[2])
+	
+	# If it was disjoint, then we return False to indicate the following list of
+	# ranges is being handled as a negative, and then we give a list of one: the
+	# input range
+	if inner == None:
+		return (False, [xmas])
+
+	# If we get here, then we know we're returning True, with at least one 
+	# range in the list. But there are between 0-2 ranges that *didn't* fit. 
+	# We'll append them to the list (if they exist) so that our intersection 
+	# range is first, and all applicable outer ranges come after.
+	ret_ranges = [inner]
+
+	# If the first of the combined tuple is the first of xmas, then it began on
+	# or before the node range.
+	if comb[0] == xmas[0]:
+		lower = (comb[0], comb[1] - 1)	# Lowest number to number before inner
+		# If the two ranges shared a start point, then comb[1]-1 == comb[0]-1 
+		# and lower[0] == lower[1] + 1. IE: there isn't anything below the inner
+		# range after all. 
+		if lower[0] < lower[1]:
+			ret_ranges.append(lower)
+
+	# Same logic in reverse for the range above the interior range: 
+	if comb[-1] == xmas[-1]:
+		upper = (comb[2] + 1, comb[3])
+		if upper[0] < upper[1]:
+			ret_ranges.append(upper)
+
+	return (True, ret_ranges)
 
 
 def do_part_two_for(lines):
+
+	graph = {}
 	for defn in lines:
 		# This time, we're *only* processing the workflow strings. 
 		if defn == "":
@@ -94,16 +154,53 @@ def do_part_two_for(lines):
 
 		wf_name, wf_body = defn.split("{")
 		wf_rule_defs = wf_body[:-1].split(",")
-		wf_rule_checks = []
 		for i, rule_def in enumerate(wf_rule_defs):
-			# The default destination at the end of the list has no test. 
-			if i == len(wf_rule_defs) -1:
-				wf_rule_checks.append((lambda obj: True, rule_def))
-				break
-			test_def, dest = rule_def.split(":")
-			wf_rule_checks.append((gen_closure(test_def), dest))
-		workflows[wf_name] = wf_rule_checks
+			graph_node(wf_name, i, rule_def, graph)
+	
+	xmas_tuples = {id: (1, 4000) for id in "xmas"}
+	all_possibilities = 4000 ** 4
+	accepted = 0
+	process_queue = deque([(("in", 0), xmas_tuples, all_possibilities)])
+	while process_queue:
+		node_address, xmas, possibilities = process_queue.popleft()
+		if node_address == ("A", 0):
+			accepted += possibilities
+			continue
+		elif node_address == ("R", 0):
+			continue
 
+		node = graph[node_address]
+		node: graph_node
+
+		xmas_range = xmas[node.checked_id]
+		scope = xmas_range[1] - xmas_range[0] + 1
+		per_capita = possibilities // scope
+		node_range = (node.lower, node.upper)
+		intersected, out_ranges = split_ranges(node_range, xmas_range)
+		
+		sanity = 0
+		if intersected:
+			this_range = out_ranges.pop(0)
+			new_scope = this_range[1] - this_range[0] + 1
+			new_possibilities = per_capita * new_scope
+			sanity += new_possibilities
+			new_xmas = xmas.copy() 
+			new_xmas[node.checked_id] = this_range
+			process_queue.append((node.dest_true, new_xmas, new_possibilities))
+		
+		for rem_out_range in out_ranges:
+			new_scope = rem_out_range[1] - rem_out_range[0] + 1
+			new_possibilities = per_capita * new_scope
+			sanity += new_possibilities
+			new_xmas = xmas.copy()
+			new_xmas[node.checked_id] = rem_out_range
+			process_queue.append((node.dest_false, new_xmas, new_possibilities))
+
+		if sanity != possibilities:
+			raise ArithmeticError("Andrew's arithmetic was bad")
+
+	return accepted
+		
 
 def solve_p1():
 	print(f"PART ONE\n--------\n")
