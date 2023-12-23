0b956eea4961acdd25da2e74f03985442b9a4dfb
ajtoms <toms@unbc.ca>
2023-12-22 22:33:48 -0800

    Solves Part 1... might be able to solve part 2

--- solutions/day23.py
+++ solutions/day23.py
@@ -15,10 +15,76 @@ except:
 	example_lines = ["Example"]
 
 def do_part_one_for(lines):
-	pass
+	grid_map = algos.vector_map_from_string_list(lines, default_fn=lambda: "#")
+	orig = vector(1, 0)
+	DEST = vector(len(lines[0]) - 2, len(lines) - 1)
+	LEGAL_STEPS = {
+		".": vector.UNIT_VECTORS(), 
+		"<": [vector.LEFT()], 
+		">": [vector.RIGHT()], 
+		"^": [vector.UP()], 
+		"v": [vector.DOWN()]
+	}
+
+	search_stack = [(orig, [vector.DOWN()], 0, set())]
+	visited = set()
+
+	def get_options(current):
+		options = []
+		for opt in LEGAL_STEPS[grid_map[current]]:
+			step = current + opt
+			tile = grid_map[step]
+			is_backtrack = step in visited
+			is_wall = tile == "#"
+			is_slope = not is_wall and len(LEGAL_STEPS[tile]) == 1
+			is_unclimable = is_slope and LEGAL_STEPS[tile][0] != opt
+			if not is_backtrack and not is_wall and not is_unclimable:
+				options.append(opt)
+		return options
+
+	best = 0
+	visited = set()
+	while search_stack:
+		current, options, path_len, visited = search_stack.pop()
+		visited.add(current)
+
+		# If we hit the end, mark it, and then backtrack
+		if current == DEST:
+			best = max(best, path_len)
+			continue
+
+		# If there are no options here, we backtrack.
+		if not options:
+			continue
+
+		# Otherwise, we remove the option we're checking from the options we'll
+		# visit if we backtrack, and pop it back on the search_stack
+		next = current + options.pop()
+		search_stack.append((current, options, path_len, visited.copy()))
+		visited.add(next)
+		current = next
+		path_len += 1
+
+		# Follow until another branch: 
+		new_options = get_options(current)
+		while len(new_options) == 1:
+			current += new_options[0]
+			new_options = get_options(current)
+			visited.add(current)
+			path_len += 1
+		
+		search_stack.append((current, new_options, path_len, visited.copy()))
+	return best
+
+
 
 def do_part_two_for(lines):
-	pass
+	new_lines = []
+	for ln in lines:
+		for slope in "><v^":
+			ln = ln.replace(slope, ".")
+		new_lines.append(ln)
+	return do_part_one_for(new_lines)
 
 def solve_p1():
 	print(f"PART ONE\n--------\n")
@@ -26,26 +92,25 @@ def solve_p1():
 
 	results = do_part_one_for(example_lines)
 	print(f"When we do part one for the example input:")
-	print(f"\tThe <THING THEY WANT> is {results}")
-	print(f"\tWe expected: <SOLUTION THEY WANT>\n")
+	print(f"\tThe longest path possible without repeats is {results}")
+	print(f"\tWe expected: 94\n")
 
 	results = do_part_one_for(input_lines)
 	print(f"When we do part one for the actual input:")
-	print(f"\tThe <THING THEY WANT> is {results}\n")
+	print(f"\tThe longest path possible without repeats is is {results}\n")
 
 def solve_p2():
-	return
 	print(f"PART TWO\n--------\n")
-	print(f"This is the prompt for Part Two of the problem.\n")
+	print(f"Do it again, but without slopes!\n")
 
 	results = do_part_two_for(example_lines)
 	print(f"When we do part two for the example input:")
-	print(f"\tThe <THING THEY WANT> is {results}")
-	print(f"\tWe expected: <SOLUTION THEY WANT>\n")
+	print(f"\tThe longest path is {results}")
+	print(f"\tWe expected: 154\n")
 
 	results = do_part_two_for(input_lines)
 	print(f"When we do part two for the actual input:")
-	print(f"\tThe <THING THEY WANT> is {results}\n")
+	print(f"\tThe longest path is {results}\n")
 
 def print_header():
-	print("--- DAY 23: <TITLE GOES HERE> ---\n")
+	print("--- DAY 23: A Long Walk ---\n")
