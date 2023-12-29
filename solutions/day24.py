8a618c31f6998050b4fc9f74936eea45bdc3b9c8
captbighead <ajt2cbhds@gmail.com>
2023-12-28 16:42:43 -0800

    SCREW NUMPY! I DID IT!

--- solutions/day24.py
+++ solutions/day24.py
@@ -8,7 +8,6 @@ from collections import defaultdict
 from collections import deque
 
 from itertools import product
-import numpy as np
 
 try:
 	input_lines = io.read_input_as_lines(24)
@@ -72,74 +71,201 @@ def do_part_one_for(line_defs, is_example):
 
 	return intersections
 
-def do_part_two_for(lines):
-	# So, I *did* code this independently and did the math by myself, but it was
-	# following someone else's solution/instructions and checking my math 
-	# against theirs. It's been a long time since my linear algebra course...
-	#
-	#	Let Rock Position = X, Y, Z		Let Hailstone i Position = x, y, z
-	#	Let Rock Velocity = A, B, C		Let Hailstone i Velocity = a, b, c
-	#	
-	#	At some time t, the new Rock Position is:
-	#
-	#		X + tA, Y + tB, Z + tC
-	#
-	#	And it intersects with some hailstone: 
-	#
-	#		x + ta, y + tb, z + tc
-	#
-	#	Using the x positions we can find an equation for t:
-	#
-	#		X + tA = x + ta
-	#		X + tA - x = ta
-	#		X - x = ta - tA
-	#		X - x = t(a - A)
-	#		t = (X - x)/(a - A)
-	#
-	#	And the same holds for y, z:
-	#
-	#		t = (Y - y)/(b - B), (Z - z)/(c - C)
-	#
-	#	So we know t in terms of x, y, so we can express an equality entirely in
-	# 	terms of x, y:
-	#
-	#		(X - x)/(a - A) = (Y - y)/(b - B)
-	#		(b - B)(X - x) = (a - A)(Y - y)
-	#		bX - bx - BX + Bx = aY - ay - AY + Ay
-	#		AY - BX = bx - ay + aY + Ay - Bx - bX
-	#		
-	#	By re-arranging the terms, the left-hand side is now independent of the
-	#	hailstone but we still don't have a constant quantity on either side. 
-	# 	However, we know that this relationship holds for any hailstone, so we 
-	# 	can substitute a the same relationship from another hailstone in for the
-	# 	AY - BX term: 
-	#
-	#		AY - BX = eu - dv + dY + Av - Bu - eX
-	#		eu - dv + dY + Av - Bu - eX = bx - ay + aY + Ay - Bx - bX
-	#		eu - dv + dY + Av - Bu - eX - bx + ay = aY + Ay - Bx - bX
-	#		eu - dv - bx + ay = aY + Ay - Bx - bX - dY - Av + Bu + eX
-	#		eX - bX + aY - dY + Ay - Av + Bu - Bx = eu - dv - bx + ay
-	#
-	#		(e - b)X + (a - d)Y + (y - v)A + (u - x)B = eu - dv - bx + ay
-	#
-	#	Simillarly, if we swap the a/x/d/u values for c/z/f/w values:
-	#
-	#		(e - b)Z + (c - f)Y + (y - v)C + (w - z)B = ew - fv - bz + cy
-	#
-	#	For any two hailstones (x, y, z, a, b, c) and (u, v, w, d, e, f). To 
-	# 	solve for X, Y, A, B, we need a system of four linear equations. We can 
-	# 	form an equation by taking any pair of known hailstones.
 
+def row_echelon_solver(matrix):
+	def is_RREF(m):
+		for i in range(len(m)):
+			for j in range(len(m[i])):
+				if i == j and m[i][j] == 1:	# RREF has i, i == 1
+					continue
+				elif j == len(m[i]) - 1:	# The final column is solns
+					continue
+				elif m[i][j]:				# All other cells must be 0 in RREF
+					return False
+		return True
+	
+	def swap_rows(m, i, j):
+		buffer = m[i]
+		m[i] = m[j]
+		m[j] = buffer
+
+	def divide_row_if_possible(m, r, n):
+		if n == 0:
+			return False
+		
+		divisible = True
+		for v in m[r]:
+			divisible = divisible and not v % n
+		if not divisible:
+			return False
+		
+		for i, v in enumerate(m[r]):
+			m[r][i] = v // n
+		return True
+
+	def apply_reductions(m, i, n, j):
+		"""Adds n * i to j"""
+		for ind, v in enumerate(m[i]):
+			m[j][ind] += n * v
+	
+	# Perform one of the three operations to get the whole shebang into RREF:
+	#
+	#	1) Swap two rows
+	#	2) Multiply a row by a non-zero number
+	# 	3) Add a multiple of one row to another.
+	#
+	# We're trying to make the leftmost column conform to RREF. So we'll choose
+	# what to do in that context. 
+	col = 0		# The column we're working on.
+	rows = len(matrix)
+	cols = len(matrix[0])
+	while not is_RREF(matrix):
+
+		if col == 4:
+			col = 0
+
+		for r, row in enumerate(matrix[col:]):
+			if row[col] < 0:
+				for i, v in enumerate(row):
+					row[i] = -v
+
+		# Check if the active column is settled:
+		is_one = matrix[col][col] == 1
+		zeroes = sum(1 if matrix[r][col] == 0 else 0 for r in range(rows))
+		if is_one and zeroes == rows - 1:
+			col += 1
+			continue
+
+		# It's not settled. If our leading number is not 1, it might be 0. If 
+		# that's the case, we need to swap in a row that has a non-zero value in
+		# this space. 
+		if not is_one and matrix[col][col] == 0:
+			# One of the rows below us works; a row above us can't. Choose a row
+			# with a 1 in it if we can.
+			best_row = -1
+			for r in range(col + 1, rows):
+				if matrix[r][col] != 0:
+					best_row = r 
+
+				# If we can slot a 1 in, do that for sure.
+				if matrix[r][col] == 1:
+					break
+			
+			# We know a best row has to exist:
+			if best_row == -1:
+				raise ValueError("Andrew is bad at math apparently")
+			
+			swap_rows(matrix, best_row, col)
+			continue
+				
+		# If we get here, we didn't swap, so we're not 0. We may not be one 
+		# though. If we aren't, check if we can evenly divide our row (we're 
+		# trying to stay with integer values)
+		if not is_one:
+			v = matrix[col][col]
+			if divide_row_if_possible(matrix, col, v):
+				continue
+		
+		# If we got here and we're still not is_one, then we couldn't divide 
+		# this row properly. Maybe one of the other rows fits the bill?
+		if not is_one:
+			did_divide = False
+			for r in range(col + 1, rows):
+				if divide_row_if_possible(matrix, r, matrix[r][col]):
+					did_divide = True
+					break
+
+			# If we did divide a different row, it is the new candidate for RREF
+			if did_divide:
+				swap_rows(matrix, col, r)
+				continue
+		
+		# If we're *still* not one and we get *here* then we couldn't make any
+		# row into 1 for this column through division or swapping alone. All 
+		# that we have left is to add some multiple of one row to another. 
+		if not is_one:
+
+			# Should be impossible, just covering my bases. 
+			if zeroes == rows - 1:
+				raise ValueError("Yup. Andrew failed to math properly.")
+
+			non_zero_rows = [r for r in range(col, rows) if matrix[r][col]]
+			performed_op = False
+			for rind, r1 in enumerate(non_zero_rows):
+				for r2 in non_zero_rows[rind + 1:]:
+					r1_lead = matrix[r1][col]
+					r2_lead = matrix[r2][col]
+
+					to_reduce = None
+					reduce_by = None
+					reductions = None
+					if abs(r1_lead % r2_lead) == 1:
+						to_reduce = r1
+						reduce_by = r2
+						reductions = r1_lead // r2_lead
+					elif abs(r2_lead % r1_lead) == 1:
+						to_reduce = r2
+						reduce_by = r1
+						reductions = r2_lead // r1_lead
+					
+					if to_reduce == None:
+						continue
+
+					apply_reductions(matrix, reduce_by, -reductions, to_reduce)
+					swap_rows(matrix, reduce_by, to_reduce)
+					performed_op = True
+					break
+
+				if performed_op:
+					break
+
+			if performed_op:
+				continue
+
+			if matrix[col + 1][col] != 0:
+				# We need to do *something* to reduce the leading edge. 
+				bigger = max((col, col + 1), key=lambda r: matrix[r][col])
+				lower = min((col, col + 1), key=lambda r: matrix[r][col])
+				n = matrix[bigger][col] // matrix[lower][col]
+				apply_reductions(matrix, lower, -n, bigger)
+				if matrix[col][col] < matrix[col+1][col]:
+					swap_rows(matrix, bigger, lower)
+				continue
+				
+			smallest_divisor = min(abs(n) for n in matrix[col] if n != 0)
+			saved = False
+			for div in range(smallest_divisor, 1, -1):
+				saved = divide_row_if_possible(matrix, col, div)
+				if saved:
+					break
+			if saved:
+				continue
+			
+		# If we're here and we haven't failed out, we're 1 in our lead position,
+		# but some rows also have values in our lead position. Because we're 1,
+		# we can obliterate them really easily!
+		if is_one:
+			non_zeroes = [r for r in range(rows) if matrix[r][col] if r != col]
+			for r in non_zeroes:
+				apply_reductions(matrix, col, -matrix[r][col], r)
+			continue
+
+		# If we get here, there's nothing more we can do. Carry on to the 
+		# next column. 
+		col += 1
+		continue
+	
+	return [matrix[r][-1] for r in range(rows)]
+
+def do_part_two_for(lines):
 	hailstones = []
 	for hail_def in lines:
 		hail = tuple(int(n) for n in hail_def.replace(" @", ",").split(","))
 		hailstones.append(hail)
 	
 	eqs = 4
-	coefficients_xy = []
-	ordinates_xy = []
-	coefficients_zy = []
-	ordinates_zy = []
+	xy_matrix = []
+	zy_matrix = []
 
 	for hail_a, hail_b in product(hailstones, repeat=2):
 		if hail_a == hail_b:
@@ -148,23 +274,122 @@ def do_part_two_for(lines):
 		u, v, w, d, e, f = hail_b
 
 		# Solves X, Y
-		coefficients_xy.append([e-b, a-d, y-v, u-x])
-		ordinates_xy.append(e*u - d*v - b*x + a*y)
+		xy_matrix.append([e-b, a-d, y-v, u-x, e*u - d*v - b*x + a*y])
 
 		# Solves Y, Z
-		coefficients_zy.append([e-b, c-f, y-v, w-z])
-		ordinates_zy.append(e*w - f*v - b*z + c*y)
+		zy_matrix.append([e-b, c-f, y-v, w-z, e*w - f*v - b*z + c*y])
 
 		eqs -= 1
 		if not eqs:
 			break
 
-	X, Y, A, B = np.linalg.solve(coefficients_xy, ordinates_xy)
-	Z, discard, C, discardagain = np.linalg.solve(coefficients_zy, ordinates_zy)
+	X, Y, A, B = row_echelon_solver(xy_matrix)
+	Z, Y, C, B = row_echelon_solver(zy_matrix)
 	rock_pos = tuple([round(v) for v in (X, Y, Z)])
 	rock_vel = tuple([round(v) for v in (A, B, C)])
 	print(f"We've found X, Y, Z, and A, B, C: {rock_pos} and {rock_vel}")
 	return sum(rock_pos)
+
+
+
+		
+
+
+
+
+#def do_part_two_for_numpy(lines):
+#	# So, I *did* code this independently and did the math by myself, but it was
+#	# following someone else's solution/instructions and checking my math 
+#	# against theirs. It's been a long time since my linear algebra course...
+#	#
+#	#	Let Rock Position = X, Y, Z		Let Hailstone i Position = x, y, z
+#	#	Let Rock Velocity = A, B, C		Let Hailstone i Velocity = a, b, c
+#	#	
+#	#	At some time t, the new Rock Position is:
+#	#
+#	#		X + tA, Y + tB, Z + tC
+#	#
+#	#	And it intersects with some hailstone: 
+#	#
+#	#		x + ta, y + tb, z + tc
+#	#
+#	#	Using the x positions we can find an equation for t:
+#	#
+#	#		X + tA = x + ta
+#	#		X + tA - x = ta
+#	#		X - x = ta - tA
+#	#		X - x = t(a - A)
+#	#		t = (X - x)/(a - A)
+#	#
+#	#	And the same holds for y, z:
+#	#
+#	#		t = (Y - y)/(b - B), (Z - z)/(c - C)
+#	#
+#	#	So we know t in terms of x, y, so we can express an equality entirely in
+#	# 	terms of x, y:
+#	#
+#	#		(X - x)/(a - A) = (Y - y)/(b - B)
+#	#		(b - B)(X - x) = (a - A)(Y - y)
+#	#		bX - bx - BX + Bx = aY - ay - AY + Ay
+#	#		AY - BX = bx - ay + aY + Ay - Bx - bX
+#	#		
+#	#	By re-arranging the terms, the left-hand side is now independent of the
+#	#	hailstone but we still don't have a constant quantity on either side. 
+#	# 	However, we know that this relationship holds for any hailstone, so we 
+#	# 	can substitute a the same relationship from another hailstone in for the
+#	# 	AY - BX term: 
+#	#
+#	#		AY - BX = eu - dv + dY + Av - Bu - eX
+#	#		eu - dv + dY + Av - Bu - eX = bx - ay + aY + Ay - Bx - bX
+#	#		eu - dv + dY + Av - Bu - eX - bx + ay = aY + Ay - Bx - bX
+#	#		eu - dv - bx + ay = aY + Ay - Bx - bX - dY - Av + Bu + eX
+#	#		eX - bX + aY - dY + Ay - Av + Bu - Bx = eu - dv - bx + ay
+#	#
+#	#		(e - b)X + (a - d)Y + (y - v)A + (u - x)B = eu - dv - bx + ay
+#	#
+#	#	Simillarly, if we swap the a/x/d/u values for c/z/f/w values:
+#	#
+#	#		(e - b)Z + (c - f)Y + (y - v)C + (w - z)B = ew - fv - bz + cy
+#	#
+#	#	For any two hailstones (x, y, z, a, b, c) and (u, v, w, d, e, f). To 
+#	# 	solve for X, Y, A, B, we need a system of four linear equations. We can 
+#	# 	form an equation by taking any pair of known hailstones.
+#
+#	hailstones = []
+#	for hail_def in lines:
+#		hail = tuple(int(n) for n in hail_def.replace(" @", ",").split(","))
+#		hailstones.append(hail)
+#	
+#	eqs = 4
+#	coefficients_xy = []
+#	ordinates_xy = []
+#	coefficients_zy = []
+#	ordinates_zy = []
+#
+#	for hail_a, hail_b in product(hailstones, repeat=2):
+#		if hail_a == hail_b:
+#			continue
+#		x, y, z, a, b, c = hail_a
+#		u, v, w, d, e, f = hail_b
+#
+#		# Solves X, Y
+#		coefficients_xy.append([e-b, a-d, y-v, u-x])
+#		ordinates_xy.append(e*u - d*v - b*x + a*y)
+#
+#		# Solves Y, Z
+#		coefficients_zy.append([e-b, c-f, y-v, w-z])
+#		ordinates_zy.append(e*w - f*v - b*z + c*y)
+#
+#		eqs -= 1
+#		if not eqs:
+#			break
+#
+#	X, Y, A, B = np.linalg.solve(coefficients_xy, ordinates_xy)
+#	Z, discard, C, discardagain = np.linalg.solve(coefficients_zy, ordinates_zy)
+#	rock_pos = (X, Y, Z)
+#	rock_vel = (A, B, C)
+#	print(f"We've found X, Y, Z, and A, B, C: {rock_pos} and {rock_vel}")
+#	return sum(rock_pos)
 
 def solve_p1():
 	print(f"PART ONE\n--------\n")
